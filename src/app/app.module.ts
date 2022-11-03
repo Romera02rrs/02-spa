edd73388eacd1055c8c8d22ab53ab29df80d528f
Romera02rrs <Romera02rrs@gmail.com>
2022-11-03 19:38:56 +0100

    Todos los heroes implementados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { APP_ROUTING } from './app.routes';
 
 // Servicios
+import { HeroesService } from './services/heroes.service';
 
 // Components
 import { AppComponent } from './app.component';
@@ -27,7 +28,9 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from 'src/app/services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes:Array<Heroe>
+
+  constructor( private _heroesService:HeroesService ) { 
+
+    this.heroes = this._heroesService.getHeroes()
+
+  }
 
   ngOnInit(): void {
+    console.log( this.heroes);
   }
 
 }
